2ba52a0b814784cd538d99c5f66b407b98472271
lion-oldriver <w879443a72cj2i@gmail.com>
2022-01-09 15:50:23 +0900

    react-tutorial_change_typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+type SquaresType = (string | null)[]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const calculateWinner = (squares: SquaresType) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+interface SquaresProps {
+  value: string | null
+  onClick: () => void
+}
+
+const Square: React.FC<SquaresProps> = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+interface BorderProps {
+  squares: SquaresType
+  onClick: (i: number) => void
+}
+
+const Board: React.FC<BorderProps> = (props) => {
+  const renderSquare = (i: number) => {
+    return (
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+interface History {
+  squares: SquaresType
+}
+
+const Game: React.FC = () => {
+  const [history, setHistory] = useState<History[]>([
+    { squares: Array(9).fill(null) }
+  ])
+  const [stepNumber, setStepNumber] = useState(0)
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const handleClick = (i: number) => {
+    const currentHistory = history.slice(0, stepNumber + 1);
+    const current = currentHistory[currentHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+      
+    setHistory([ ...currentHistory, { squares: squares } ])
+    setStepNumber(currentHistory.length)
+    setXIsNext(!xIsNext)
+  }
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step)
+    setXIsNext((step % 2) === 0)
+  }
+  
+  const currentHistory = [...history]
+  const current = currentHistory[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = currentHistory.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  const status = winner ?
+    "Winner: " + winner
+    :
+    "Next player: " + (xIsNext ? "X" : "O");
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={i => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol start={0}>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
